a00f5f94096c49b0953642675de403d76ae6b208
Matheus Tambosi <matheustambosi@gmail.com>
2020-09-17 21:18:30 -0300

    Layout mobile estilizado

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <title>Pac2</title>
 </head>
-<body>
 
+<body>
+    <main>
+        <div class="container">
+            <p>Informe seus Dados</p>
+            <div class="inputs">
+                <label>Usuário</label>
+                <input type="text">
+                <label>Senha</label>
+                <input type="password">
+            </div>
+            <button>Entrar</button>
+        </div>
+    </main>
 </body>
+
 </html>
