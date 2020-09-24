5c7d749119b7e7a35a33ccd459996b2f8e9c71b3
jvst1 <61890677+jvst1@users.noreply.github.com>
2020-09-24 20:42:40 -0300

    Remake

--- index.php
+++ index.php
@@ -4,25 +4,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css" media="screen,projection" />
     <link rel="stylesheet" href="css/style.css">
+
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <title>Pac2</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>ECommerce Pac2</title>
 </head>
 
+<script>
+    $(document).ready(function() {
+        $('.sidenav').sidenav();
+    });
+</script>
+
 <body>
-    <main>
-        <div class="container">
-            <p>Informe seus Dados</p>
-            <div class="inputs">
-                <label>Usuário</label>
-                <input type="text">
-                <label>Senha</label>
-                <input type="password">
-            </div>
-            <button>Entrar</button>
-            <a href="cadastro.php"><button>Cadastrar</button></a>
+
+    <nav class="blue" style="padding:0px 10px; position: fixed;">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Pac2</a>
+
+            <a href="#" class="sidenav-trigger" data-target="mobile-nav">
+                <i class="material-icons">menu</i>
+            </a>
+
+            <ul class="right hide-on-med-and-down ">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Video</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
         </div>
+    </nav>
+
+
+    <ul class="sidenav" id="mobile-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Video</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+
+    <main>
+        teste
     </main>
+
 </body>
 
 </html>
